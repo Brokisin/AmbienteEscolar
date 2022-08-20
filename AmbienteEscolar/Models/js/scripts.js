fb3aad0e76d94373ee9b7dcf241766047937a58c
Ismael P.F <ismaelp.f@outlook.com>
2022-08-20 16:41:26 -0300

    Adicionar arquivos de projeto.

---
+++ AmbienteEscolar/Models/js/scripts.js
@@ -0,0 +1,37 @@
+﻿
+function botaoInvisible() {
+    let botaoMenu = document.getElementById("linkMenu").style.visibility = "hidden";
+}
+
+function executarLogin() {
+    botaoInvisible();
+    let usuario = document.getElementById("usuario");
+    let senha = document.getElementById("senha");
+
+    let SenhaVazia = (senha.value == "" || senha.value == null);
+    let UsuarioVazio = (usuario.value == "" || usuario.value == null)
+
+    let IsNullOrEmpty = (UsuarioVazio || SenhaVazia);
+
+    if (IsNullOrEmpty) {
+        document.getElementById("confirmacao").innerHTML = "Usuário ou senha inválidos.";
+    }
+    
+    return alert("Bem-vindo, " + usuario.value + "!");
+
+    document.getElementById("linkMenu").style.visibility = "visible";
+}
+
+function formularioAlerta() {
+    botaoInvisible();
+    var usuario = document.getElementById("usuario");
+    var senha = document.getElementById("senha");
+
+    let charsPreenchidos = (usuario.value != "" & senha.value != "");
+
+    if (charsPreenchidos) {
+        document.getElementById("confirmacao").innerHTML = "Logue.";
+    } else {
+        document.getElementById("confirmacao").innerHTML = "Preencha todos os campos.";
+    }
+}
