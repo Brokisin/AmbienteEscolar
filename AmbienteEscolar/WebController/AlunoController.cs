22ab65173122db19219e8a039310e4ad98eef49d
Eros <eroskarsburg@gmail.com>
2022-09-10 16:19:02 -0300

    commit doido

---
+++ AmbienteEscolar/WebController/AlunoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmbienteEscolar.WebController
+{
+    public class AlunoController
+    {
+        public ActionResult InsertAluno()
+        {
+
+        }
+    }
+}
