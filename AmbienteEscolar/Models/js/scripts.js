3cbc16dc5a4c27abd5f81189fa6038412e5db2f8
dev_Eros <eros.rosa@rodosoft.com.br>
2022-10-31 10:08:08 -0300

    Att

--- AmbienteEscolar/Models/js/scripts.js
+++ AmbienteEscolar/Models/js/scripts.js
@@ -40,3 +40,20 @@ function limparCampos() {
     document.getElementById("nome").value = "";
     document.getElementById("email").value = "";
 }
+
+//==========
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 10000); // Change image every 10 seconds
+}
